dcb59c05c04f20c29a24fc49f9499dc08867e048
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-06-10 13:12:21 +0300

    swiper styles finished/other small changes

--- src/components/FeedBack/FeedBack.jsx
+++ src/components/FeedBack/FeedBack.jsx
@@ -4,69 +4,83 @@ import StyledtWrapper from 'components/UI/StyledWrapper/StyledWrapper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import avatar from '../../assets/images/FeedBack/avatar1.png';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
+import css from './FeedBack.module.css';
+
 import FeedBackItem from './FeedBackItem';
 const FeedBack = () => {
   return (
     <StyledtWrapper>
       <div className="container">
         <Title text="Client’s Feedback" />
-        <Swiper
-          modules={[Pagination]}
-          spaceBetween={60}
-          slidesPerView={3}
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={swiper => console.log(swiper)}
-        >
-          <SwiperSlide>
-            <FeedBackItem
-              feedBackText="This a great 
+        <div className={css.sliderWrapper}>
+          <Swiper
+            style={{
+              '--swiper-pagination-color': '#FF4300',
+              '--swiper-pagination-bullet-inactive-color':
+                '#ff620180',
+              '--swiper-pagination-bullet-inactive-opacity': '1',
+              '--swiper-pagination-bottom': '8px',
+              '--swiper-pagination-bullet-width': '12px',
+              '--swiper-pagination-bullet-height': '8px',
+              '--swiper-pagination-bullet-border-radius': 'none',
+              '--swiper-pagination-bullet-horizontal-gap': '2px',
+            }}
+            modules={[Pagination]}
+            spaceBetween={60}
+            slidesPerView={3}
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={swiper => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <FeedBackItem
+                feedBackText="This a great 
         historic site to visit. I rented
          a little cart and the driver was a 
          really good friendl"
-              starsCount={5}
-              name="Brooklyn Simmons"
-              avatar={avatar}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <FeedBackItem
-              feedBackText="This a great 
+                starsCount={5}
+                name="Brooklyn Simmons"
+                avatar={avatar}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <FeedBackItem
+                feedBackText="This a great 
         historic site to visit. I rented
          a little cart and the driver was a 
          really good friendl"
-              starsCount={5}
-              name="Brooklyn Simmons"
-              avatar={avatar}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <FeedBackItem
-              feedBackText="This a great 
+                starsCount={5}
+                name="Brooklyn Simmons"
+                avatar={avatar}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <FeedBackItem
+                feedBackText="This a great 
         historic site to visit. I rented
          a little cart and the driver was a 
          really good friendl"
-              starsCount={5}
-              name="Brooklyn Simmons"
-              avatar={avatar}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <FeedBackItem
-              feedBackText="This a great 
+                starsCount={5}
+                name="Brooklyn Simmons"
+                avatar={avatar}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <FeedBackItem
+                feedBackText="This a great 
         historic site to visit. I rented
          a little cart and the driver was a 
          really good friendl"
-              starsCount={5}
-              name="Brooklyn Simmons"
-              avatar={avatar}
-            />
-          </SwiperSlide>
-        </Swiper>
+                starsCount={5}
+                name="Brooklyn Simmons"
+                avatar={avatar}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </StyledtWrapper>
   );
